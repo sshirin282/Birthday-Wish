a69c22bac579c10077334de3ed870d2f0e6a785a
azhar <shaikhnoorish8@gmail.com>
2021-09-16 16:50:45 +0530

    Birthday Wish

--- app/src/main/java/com/example/birthday/Activity2.kt
+++ app/src/main/java/com/example/birthday/Activity2.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 
 class Activity2 : AppCompatActivity() {
     lateinit var textview:TextView
+    lateinit var textview1:TextView
 
 companion object{
 const val NAME_EXTRA="name_extra"
@@ -14,8 +15,10 @@ const val NAME_EXTRA="name_extra"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
         textview=findViewById(R.id.birthdayGreeting)
+        textview1=findViewById(R.id.birthdayGreeting2)
 
         val name= intent.getStringExtra(NAME_EXTRA)
         textview.text = "HAPPY BIRTHDAY TO\n$name!"
+        textview1.text="From Shirin"
     }
 }
